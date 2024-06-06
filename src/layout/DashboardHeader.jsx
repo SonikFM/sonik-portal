a405889b1955d37d5083cb45546e3fc10050dc09
Qasim Fida <qasimfida0054@gmail.com>
2024-06-06 19:09:49 +0500

    feat[sonik-12]-event-list

--- src/layout/DashboardHeader.jsx
+++ src/layout/DashboardHeader.jsx
@@ -2,14 +2,21 @@ import { useGetTodosQuery } from "@/apis/todos";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
-import CalendarIcon from "@/svgs/CalendarIcon";
 import SearchIcon from "@/svgs/SearchIcon";
 import BellIcon from "@/svgs/BellIcon";
 import CalendarCheckIcon from "@/svgs/CalendarCheckIcon";
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
 
-export const DashboardHeader = ({ hasActionButton=false, title, description, icon }) => {
+export const DashboardHeader = ({
+	hasActionButton = false,
+	title,
+	description,
+	icon,
+	hasNotifications = true,
+	hasSearch = true,
+	children
+}) => {
 	const state = useSelector((state) => state.app);
 	const { data, error, isLoading } = useGetTodosQuery();
 	const { i18n } = useTranslation();
@@ -21,33 +28,42 @@ export const DashboardHeader = ({ hasActionButton=false, title, description, ico
 		<div className=" fixed top-0 left-[272px] w-[calc(100%-272px)] bg-grey-dark z-50  px-8">
 			<div className="border-b border-grey-light flex items-center justify-between h-[88px] gap-5 bg-grey-dark w-full">
 				<div className="flex items-center gap-3 shrink-0">
-					<div className="flex items-center justify-center w-12 h-12 border rounded-full border-grey-light shrink-0">
-						{icon}
-					</div>
+					{icon && (
+						<div className="flex items-center justify-center w-12 h-12 border rounded-full border-grey-light shrink-0">
+							{icon}
+						</div>
+					)}
 					<div>
 						<h4 className="text-lg font-medium leading-5 text-white">
 							{title}
 						</h4>
-						<p className="mt-1 text-sm text-grey-100">{description}</p>
+						{description && (
+							<p className="mt-1 text-sm text-grey-100">{description}</p>
+						)}
 					</div>
 				</div>
 				<div className="flex gap-3 text-grey-100">
-					<div className="flex items-center justify-center w-10 shrink-0">
-						<SearchIcon />
-					</div>
-					<div className="flex items-center justify-center w-10 shrink-0">
-						<BellIcon />
-					</div>
-					{hasActionButton && (
-						<>
-							<Button variant="outline" className="flex gap-1 bg-transparent">
-								<CalendarCheckIcon /> Schedule
-							</Button>
-							<Button className="flex gap-1">
-								<PlusIcon /> New Event
-							</Button>
-						</>
+					{hasSearch && (
+						<div className="flex items-center justify-center w-10 shrink-0">
+							<SearchIcon />
+						</div>
 					)}
+					{hasNotifications && (
+						<div className="flex items-center justify-center w-10 shrink-0">
+							<BellIcon />
+						</div>
+					)}
+					{children ||
+						(hasActionButton && (
+							<>
+								<Button variant="outline" className="flex gap-1 bg-transparent">
+									<CalendarCheckIcon /> Schedule
+								</Button>
+								<Button className="flex gap-1">
+									<PlusIcon /> New Event
+								</Button>
+							</>
+						))}
 				</div>
 			</div>
 		</div>
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,42 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const generatePageNumbers = (table) => {
+	const totalPages = table.getPageCount();
+	const currentPage = table.getState().pagination.pageIndex + 1;
+	const pages = [];
+
+	if (totalPages <= 5) {
+		for (let i = 1; i <= totalPages; i++) {
+			pages.push(i);
+		}
+	} else {
+		if (currentPage <= 3) {
+			pages.push(1, 2, 3, 4, "...", totalPages);
+		} else if (currentPage > 3 && currentPage < totalPages - 2) {
+			pages.push(
+				1,
+				"...",
+				currentPage - 1,
+				currentPage,
+				currentPage + 1,
+				"...",
+				totalPages
+			);
+		} else {
+			pages.push(
+				1,
+				"...",
+				totalPages - 3,
+				totalPages - 2,
+				totalPages - 1,
+				totalPages
+			);
+		}
+	}
+	return pages;
+};
