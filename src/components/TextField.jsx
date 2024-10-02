b4be1892c606a8d8c1352e2ca608f0080ce49021
FakharAbbas5 <mfakharabbas7@gmail.com>
2024-10-02 13:56:54 +0500

    feat: SelectField component and modified Input to match design

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -3,9 +3,11 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { twMerge } from "tailwind-merge";
 import InformationIcon from "@/svgs/InformationIcon";
+import { Textarea } from "./ui/textarea";
 
 const TextField = forwardRef(
   ({ className, type, label, Icon, required, placeholder, ...props }, ref) => {
+    const InputField = type !== "textarea" ? Input : Textarea;
     return (
       <div className="w-full flex flex-col gap-2">
         <Label className="text-white">
@@ -13,13 +15,15 @@ const TextField = forwardRef(
         </Label>
 
         <div className="w-full relative">
-          <Input
+          <InputField
             className={twMerge(
               "text-white bg-transparent border-grey-light placeholder:text-grey-100 rounded-lg",
               Icon && "px-10",
             )}
             type={type}
             placeholder={placeholder}
+            {...props}
+            ref={ref}
           />
           {Icon && (
             <Icon className="absolute top-1/2 -translate-y-1/2 left-3 w-4 text-grey" />
--- src/containers/Event/elements/BasicInfo.jsx
+++ src/containers/Event/elements/BasicInfo.jsx
@@ -1,18 +1,47 @@
 import SelectField from "@/components/SelectField";
 import TextField from "@/components/TextField";
+import { Search } from "lucide-react";
 
 const BasicInfo = () => {
   return (
-    <div className="space-y-8 w-full">
+    <div className="space-y-6 w-full">
       <TextField label="Title" required={true} placeholder="Add Title" />
       <SelectField
         label="Type"
+        placeholder="Choose the type of event"
         required={true}
         options={[
           { value: "concert", label: "Concert" },
           { value: "dj", label: "DJ" },
           { value: "party", label: "Party" },
         ]}
+      />
+      <TextField
+        label="Description"
+        type="textarea"
+        placeholder="Add Description"
+      />
+      <SelectField
+        label="Privacy"
+        required={true}
+        placeholder="Select type"
+        options={[
+          { value: "public", label: "Public" },
+          { value: "hidden", label: "Hidden" },
+          { value: "private", label: "Private" },
+        ]}
+      />
+      <SelectField
+        label="Venue"
+        required={true}
+        hasSearch={true}
+        placeholder="Find a venue"
+        options={[
+          { value: "public", label: "Public" },
+          { value: "hidden", label: "Hidden" },
+          { value: "private", label: "Private" },
+        ]}
+        Icon={Search}
       />
     </div>
   );
