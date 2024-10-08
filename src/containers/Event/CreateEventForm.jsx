d2ccc17950d4f93369755fe4ffe2b2b14be7d5cd
FakharAbbas5 <mfakharabbas7@gmail.com>
2024-10-08 21:48:58 +0500

    feat: Loading component

--- src/containers/Event/CreateEventForm.jsx
+++ src/containers/Event/CreateEventForm.jsx
@@ -2,10 +2,9 @@ import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import useEventHelper from "./hooks/useEventHelper";
-import steps from "./steps/config";
 
 const CreateEventForm = ({ children, activeStep, setActiveStep }) => {
-  const { getInitialState, submitEvent, isSuccess } = useEventHelper({
+  const { getInitialState, submitEvent } = useEventHelper({
     activeStep: activeStep.id,
   });
 
@@ -31,11 +30,6 @@ const CreateEventForm = ({ children, activeStep, setActiveStep }) => {
   const onSubmit = data => {
     submitEvent(data);
   };
-
-  useEffect(() => {
-    console.log(isSuccess, "isSuccess");
-    if (isSuccess && activeStep.id < 5) setActiveStep(steps[activeStep.id]);
-  }, [isSuccess]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
